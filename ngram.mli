a529c3092c215868227d26d27524c3df9616065a
Mason Albert <malber20@jhu.edu>
2023-11-13 14:29:02 -0500

    Added comments to ngram.mli

--- ngram.mli
+++ ngram.mli
@@ -1,32 +1,21 @@
-(* Boggle Module *)
-  (* fun API call wrapper *)
-  (* fun Create Trie *)
-  (* fun Process player turn *)
-  (* fun Compute all player scores *)
-  (* fun Get hint (based on hint type) *)
-  (* fun/module Board generation *)
-(* Trie Module *)
-  (* fun Insert word*)
-  (* fun Lookup word*)
-
-
-(*
-1. Grab all dict words
-2. Compute 2-grams
-3. Generate board with random weights + common letter pairs
-4. Generate Trie with all words
-5. User 1 turn:
-    Enter words
-6. Other users' turns
-7. Output each user's score and which words counted and why
+(* 
+   This module will be used to create a bi-gram distribution over the english language.
+   For example, over the words ["app"; "apple"; "application"], the following distribution would be generated:
+      (
+        ('a', [('p', 3); ('t', 1)]),
+        ('p', [('p', 3); ('l', 2)]),
+        ('l', [('e', 1); ('i', 1)]),
+        ...
+      )
+  This will be used to generate a reasonable Boggle board, with a variety of possible words, rather than
+  random nonsense with no possibilities.
 *)
 
-
 module Ngram: sig
-  type t
-  val make_distribution: string list -> t
-  val get_random: t -> string
-  val get_next: string -> string
+  type t (* the map holding the distribution *)
+  val make_distribution: string list -> t (* generate the distribution from a list of strings *)
+  val get_random: t -> char (* get single letter weighted by frequency in the english language *)
+  val get_next: char -> char (* get a random second letter in the bigram given the prefix, weighted by frequency *)
 end
 
 
