c324b0e2133d267e2035de01ff90a415ad77b7e8
Mason Albert <malber20@jhu.edu>
2023-11-16 16:20:09 -0500

    Finished trie.mli

--- src/trie.mli
+++ src/trie.mli
@@ -1,5 +1,23 @@
+(* Trie module (prefix tree) to be used for storing words and efficiently computing which words can be found on the board *)
+(* 
+  If we insert words "app", "apple", and "append", the trie would be as follows:
+  
+                            'a'
+                            /
+                          'p' 
+                           |
+           (is_word=true) 'p'
+                         /   \
+                       'l'   'e'
+                        |     |
+        (is_word=true) 'e'   'n'
+                              |
+                             'd' (is_word=true)
+
+*)
 module Trie: sig
-  type t
-  val insert: t -> string -> t
-  val is_word: t -> string -> bool
+  type t (* Trie data type, likely a record like { is_word: bool; children: (char, t, char_cmp) Map.t } *)
+  val insert: t -> string -> t (* Inserts a word into the trie *)
+  val is_word: t -> string -> bool (* Check if a given word exists in the trie *)
+  val get_child: t -> char -> t option (* return the child of the root node that char maps to (wrapped in Some) if it exists, else None *)
 end
