22ae56255078084c2054336808871d5c464b96ef
Edward Wang <ewang43@jhu.edu>
2023-11-29 15:28:22 -0500

    Implement get_definition

---
+++ src/dictionary.ml
@@ -0,0 +1,30 @@
+open Core
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
+
+module Dictionary = struct
+  let get_all_words _ = []
+
+  let get_definition (word : string) : string option =
+    let call =
+      Client.get
+        (Uri.of_string
+           ("https://api.dictionaryapi.dev/api/v2/entries/en/" ^ word))
+      >>= fun (resp, body) ->
+      match resp |> Response.status |> Code.code_of_status with
+      | 200 ->
+          body |> Cohttp_lwt.Body.to_string >|= fun body ->
+          let json =
+            Yojson.Basic.(body |> from_string |> Util.to_list |> List.hd_exn)
+          in
+          let first_meaning = Yojson.Basic.Util.(json |> member "meanings" |> to_list |> List.hd_exn) in
+          let first_definition = Yojson.Basic.Util.(first_meaning |> member "definitions" |> to_list |> List.hd_exn) in
+          let definition = Yojson.Basic.Util.(first_definition |> member "definition" |> to_string) in
+          Some definition
+      | _ -> Lwt.return_none
+    in
+    Lwt_main.run call
+
+  let get_definitions (_ : string list) = []
+end
--- src/dictionary.mli
+++ src/dictionary.mli
@@ -1,6 +1,6 @@
 (* This module is a wrapper to interact with the Merriam-Webster dictionary API using Cohttp*)
-
 module Dictionary: sig
   val get_all_words: unit -> string list (* Get all words in the english language *)
+  val get_definition: string -> string option
   val get_definitions: string list -> string list (* Get definitions for selected words (those that we're giving hints for) *)
 end
